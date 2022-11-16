4acf4d88fd3c9d93c9f35243584cac3409454867
Danny van Duist <dvduist@hotmail.nl>
2022-11-16 21:58:55 +0100

    Add a script to generate a .raw file with profiler data and activate it at certain points during the flythrough animation

---
+++ OcclusionCullingTest/Assets/1. Scripts/Analysis/ProfilerUtil.cs
@@ -0,0 +1,69 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.SceneManagement;
+using VDFramework;
+
+namespace Analysis
+{
+	public class ProfilerUtil : BetterMonoBehaviour
+	{
+		private byte recordingID = 0;
+
+		private string filePath = "Profiles/";
+		private string sceneName = "CurrentScene";
+
+		private bool occlusionCullingEnabled;
+
+		private string OcclusionCulling => occlusionCullingEnabled ? "ON" : "OFF";
+
+		private void Awake()
+		{
+			recordingID      = 0;
+			Profiler.enabled = false;
+			sceneName        = SceneManager.GetActiveScene().name;
+
+			occlusionCullingEnabled = Camera.main.useOcclusionCulling;
+
+			filePath += $"{sceneName}[{OcclusionCulling}]/";
+
+			if (!Directory.Exists(filePath))
+			{
+				Directory.CreateDirectory(filePath);
+			}
+		}
+
+		public void StartProfiling(string logName)
+		{
+			++recordingID;
+			Profiler.logFile         = filePath + $"{recordingID}_{logName}";
+			Profiler.enableBinaryLog = true;
+
+			Profiler.enabled = true;
+
+#if UNITY_EDITOR
+
+			// Apparently needed in the editor if you don't have the profiler window open
+			UnityEditorInternal.ProfilerDriver.enabled = true;
+#endif
+		}
+
+		public void EndProfiling()
+		{
+#if UNITY_EDITOR
+
+			// Apparently needed in the editor if you don't have the profiler window open
+			UnityEditorInternal.ProfilerDriver.enabled = false;
+#endif
+
+			Profiler.enabled         = false;
+			Profiler.enableBinaryLog = false;
+			Profiler.logFile         = string.Empty;
+		}
+
+		private void OnDestroy()
+		{
+			EndProfiling();
+		}
+	}
+}
