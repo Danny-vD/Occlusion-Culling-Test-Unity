9ea90591383eb2b0363f30e75c1abb8b5ddeae33
Danny van Duist <dvduist@hotmail.nl>
2022-11-17 02:19:31 +0100

    Setup a fixed angle camera for measuring performance different for specific angles

---
+++ OcclusionCullingTest/Assets/1. Scripts/Analysis/SingleProfilerFileRecorder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Analysis
+{
+	public class SingleProfilerFileRecorder : ProfilerUtil
+	{
+		[SerializeField]
+		private string fileName = "PROFILE.raw";
+		
+		// Animation system does not support overloaded functions
+		public void StartRecordingData()
+		{
+			StartProfiling(fileName);
+		}
+	}
+}
---
+++ OcclusionCullingTest/Assets/1. Scripts/Utility/ApplicationUtil.cs
@@ -0,0 +1,17 @@
+﻿using VDFramework;
+
+namespace Utility
+{
+	public class ApplicationUtil : BetterMonoBehaviour
+	{
+		public void ExitApplication()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.ExitPlaymode();
+			return;
+#endif
+			
+			UnityEngine.Application.Quit();
+		}
+	}
+}
